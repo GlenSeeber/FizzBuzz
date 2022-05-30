9a43ab9fe5b93f5fe04330308bfa59db3dacf56e
Glen Seeber <gseeber5@gmail.com>
2022-05-29 23:32:09 -0700

    Nice-ification - made more variables so it's easier to update the script in the future - expanded upon comments where necessary - removed reduntant terminal command (txt solution shows proficiency without     needing to do something that is essentially a copy paste)

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
-# 3     ->   fizz
-# 5     ->   buzz
-# 3 & 5 ->   fizzbuzz
-#-----------------------
+#funcs
 
-
-
-def terminal():
-    for i in range(1, 101):
-        #print the number
-        print(i, end=' ')
-        #fizz
-        if i % 3 == 0:
-            print("Fizz", end='')
-        #buzz
-        if i % 5 == 0:
-            print("Buzz", end='')
-
-        # newline
-        print('')
-
-def txtDoc():
+def txtDoc(fizzNum, buzzNum, fileName):
     #our txt document variable
     myDoc = ''
 
     for i in range(1, 101):
         #print numbers
         myDoc += (str(i) + ' ')
-        # Fizz
-        if i % 3 == 0:
+        # if current number is divisible by fizzNum (default: 3), print Fizz
+        if i % fizzNum == 0:
             myDoc += "Fizz"
-        # Buzz
-        if i % 5 == 0:
+        # if current number is divisible by buzzNum (default: 5), print Buzz
+        if i % buzzNum == 0:
             myDoc += "Buzz"
 
         # newline
         myDoc += "\n"
         
     # Save to .txt
-    f = open('fizzbuzz.txt', 'w')
+    f = open(fileName+'.txt', 'w')
     f.write(myDoc)
     f.close()
 
-txtDoc()
+
+# actual program
+
+fizz = 3
+buzz = 5
+
+txtDoc(fizz, buzz, 'output')
